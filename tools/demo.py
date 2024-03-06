d7612d97ebf2e35d57b673db6bf4c389058244e9
Anak1st <xiafan8050@outlook.com>
2024-03-06 13:37:41 +0800

    fix path on windows

--- tools/demo.py
+++ tools/demo.py
@@ -1,17 +1,25 @@
 from __future__ import absolute_import
 
 import os
+import sys
+abs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(abs_path)
+print(abs_path)
+
+
 import glob
 import numpy as np
+
 
 from siamfc import TrackerSiamFC
 
 
 if __name__ == '__main__':
-    seq_dir = os.path.expanduser('~/data/OTB/Crossing/')
+    root_dir = 'D:/Downloads/Datasets/OTB100/data'
+    seq_dir = os.path.join(root_dir, 'Crossing/')
     img_files = sorted(glob.glob(seq_dir + 'img/*.jpg'))
     anno = np.loadtxt(seq_dir + 'groundtruth_rect.txt')
     
-    net_path = 'pretrained/siamfc_alexnet_e50.pth'
+    net_path = 'D:/Repos/siamfc-pytorch/model/siamfc_alexnet_e50.pth'
     tracker = TrackerSiamFC(net_path=net_path)
     tracker.track(img_files, anno[0], visualize=True)
--- tools/test.py
+++ tools/test.py
@@ -1,16 +1,21 @@
 from __future__ import absolute_import
 
 import os
-from got10k.experiments import *
+import sys
+abs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(abs_path)
+print(abs_path)
 
+
+from got10k.experiments import *
 from siamfc import TrackerSiamFC
 
 
 if __name__ == '__main__':
-    net_path = 'pretrained/siamfc_alexnet_e50.pth'
+    net_path = 'D:/Repos/siamfc-pytorch/model/siamfc_alexnet_e50.pth'
     tracker = TrackerSiamFC(net_path=net_path)
 
-    root_dir = os.path.expanduser('~/data/OTB')
+    root_dir = 'D:/Downloads/Datasets/OTB100/data'
     e = ExperimentOTB(root_dir, version=2015)
-    e.run(tracker)
+    e.run(tracker, visualize=True)
     e.report([tracker.name])
